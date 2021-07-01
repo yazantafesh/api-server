3c00e9fecedeee1cf045b91b62f86c8481482764
yazantafesh <yazantafesh1@gmail.com>
2021-07-01 13:50:50 +0300

    done

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 const server = require('./src/server');
 const mongoose = require('mongoose');
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 const MONGODB_URI = process.env.MONGODB_URI;
 
 mongoose
