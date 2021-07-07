e3626fea3b676ffb06fcac7e33487da054176fff
yazantafesh <yazantafesh1@gmail.com>
2021-07-07 17:08:21 +0300

    done last

--- pool.js
+++ pool.js
@@ -4,9 +4,6 @@ require('dotenv').config();
 
 const connectionString = 'postgres://amnzdvktpklsci:a98fc677e12461dd27be17d90b031067c66855ffd8e5964228e6b887c33b4c54@ec2-52-19-170-215.eu-west-1.compute.amazonaws.com:5432/dd1rb7amso6vui' || process.env.DATABASE_URL;
 
-// const url = process.env.DATABASE_URL || 'postgres://amnzdvktpklsci:a98fc677e12461dd27be17d90b031067c66855ffd8e5964228e6b887c33b4c54@ec2-52-19-170-215.eu-west-1.compute.amazonaws.com:5432/dd1rb7amso6vui'
-
-// pg.defaults.ssl = process.env.NODE_ENV === 'production' && { rejectUnauthorized: false };
 
 if (!process.env.DATABASE_URL) {
   module.exports = new pg.Client({
