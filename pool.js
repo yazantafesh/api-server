245c40fc4b112f64c004ba9366fdb2af577eaa06
yazantafesh <yazantafesh1@gmail.com>
2021-07-07 16:53:41 +0300

    conString

--- pool.js
+++ pool.js
@@ -2,7 +2,9 @@ const pg = require('pg');
 
 require('dotenv').config();
 
-const url = process.env.DATABASE_URL || 'postgres://amnzdvktpklsci:a98fc677e12461dd27be17d90b031067c66855ffd8e5964228e6b887c33b4c54@ec2-52-19-170-215.eu-west-1.compute.amazonaws.com:5432/dd1rb7amso6vui'
+const connectionString = 'postgres://amnzdvktpklsci:a98fc677e12461dd27be17d90b031067c66855ffd8e5964228e6b887c33b4c54@ec2-52-19-170-215.eu-west-1.compute.amazonaws.com:5432/dd1rb7amso6vui' || process.env.DATABASE_URL;
+
+// const url = process.env.DATABASE_URL || 'postgres://amnzdvktpklsci:a98fc677e12461dd27be17d90b031067c66855ffd8e5964228e6b887c33b4c54@ec2-52-19-170-215.eu-west-1.compute.amazonaws.com:5432/dd1rb7amso6vui'
 
 // pg.defaults.ssl = process.env.NODE_ENV === 'production' && { rejectUnauthorized: false };
 
